4ffd9ff82bfd2ffb6ea92b64021f464d9d8e109b
Ahmet Yusuf Demir <ahmetyusufdmr88@gmail.com>
2024-01-17 22:01:03 +0300

    Big Ui Changes

--- scripts/main.js
+++ scripts/main.js
@@ -19,7 +19,7 @@ function createWindow() {
   // open dev tools
   mainWindow.webContents.openDevTools();
 
-  mainWindow.loadFile('../html/index.html');
+  mainWindow.loadFile('../html/mainpage.html');
 
   mainWindow.on('closed', function () {
     mainWindow = null;
